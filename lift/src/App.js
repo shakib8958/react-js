7a63df17dae32a7f699427bca561eab083bba3ae
Shakib <sakib895886@gmail.com>
2022-10-26 16:59:34 +0530

    first

---
+++ lift/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+
+    <h1> hlw everyone this side shakib here </h1>
+    </>
+  );
+}
+
+export default App;
