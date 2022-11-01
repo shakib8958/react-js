b7e71ceb32ffc31250a5864cce687b8e3c304b78
Shakib <sakib895886@gmail.com>
2022-11-01 15:52:32 +0530

    form

---
+++ form/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  
+  return (
+    
+    <div className="App">
+
+      <h1> Hnadle the form of react js fill the form</h1>
+      <form>
+
+      <input type="text" placeholder='enter the name'/>
+      <br></br>
+      <select>
+      <option> slect the option </option>
+      <option> movie name </option>
+      <option>  marvel </option>
+      <option> dc </option>
+      <input type="checkbox"/> <span> Accept the form</span>
+      <br>
+      </br>
+      <h2> how to pay </h2>
+      <h2> online</h2>
+      
+  
+      <button type="submit"> submit </button>
+      
+      
+      </select>
+      </form>
+    </div>
+  );
+}
+
+export default App;
